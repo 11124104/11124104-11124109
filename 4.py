7718c7dec3f9fd78037cd80b47e16ea9e0b3dc90
11124104 <113520740+11124104@users.noreply.github.com>
2024-06-04 17:53:41 +0800

    Update 20240604.py

--- 4.py
+++ 4.py
@@ -1,10 +1,6 @@
-import math
-mark={'A':84,'B':90,'C':75,'D':82}
-print("最高分為：")
-print(max(mark.values()))
-print("最低分為：")
-print(min(mark.values()))
-print("人數為：")
-print(len(mark))
-print("平均分：")
-print(sum(mark.values())/len(mark))
+year = int(input())
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+    print(True)
+else:
+    print(False)
